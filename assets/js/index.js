3e0df7f8c82e8c4f9349c6a7e7a3a92a2b498471
JustineDelaRosa <60209838+JustineDelaRosa@users.noreply.github.com>
2022-11-09 13:25:00 +0800

    Update Responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
 
-const textArray = ["Justine Dela Rosa.", "A Web Developer"];
+const textArray = ['Justine Dela Rosa.', 'A Web Developer'];
 const typingDelay = 150;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
@@ -10,21 +10,21 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
+    if (!cursorSpan.classList.contains('typing'))
+      cursorSpan.classList.add('typing');
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
   } else {
-    cursorSpan.classList.remove("typing");
+    cursorSpan.classList.remove('typing');
     setTimeout(erase, newTextDelay);
   }
 }
 
 function erase() {
   if (charIndex > 0) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
+    if (!cursorSpan.classList.contains('typing'))
+      cursorSpan.classList.add('typing');
     typedTextSpan.textContent = textArray[textArrayIndex].substring(
       0,
       charIndex - 1
@@ -32,14 +32,29 @@ function erase() {
     charIndex--;
     setTimeout(erase, erasingDelay);
   } else {
-    cursorSpan.classList.remove("typing");
+    cursorSpan.classList.remove('typing');
     textArrayIndex++;
     if (textArrayIndex >= textArray.length) textArrayIndex = 0;
     setTimeout(type, typingDelay + 1100);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  // On DOM Load initiate the effect
+document.addEventListener('DOMContentLoaded', function () {
   if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// Smooth Scrolling
+$('#navbar a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      1000
+    );
+  }
 });
